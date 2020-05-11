6a0906b0ad4be24613c42b3eece0e8004a56ca31
Dave Knopp <dave@stellacore.com>
2020-05-11 16:19:38 -0600

    runBuildDemo.sh graphviz generator and a few comments

--- runBuildDemo.sh
+++ runBuildDemo.sh
@@ -2,17 +2,30 @@
 # Example command
 #
 
+# create and configure build environment
 mkdir ./build ./tmplocal
 cd build
-cmake -DCMAKE_INSTALL_PREFIX=../tmplocal/ ..
 # would generally run ccmake here to setup configuration (use defaults here)
+cmake -DCMAKE_INSTALL_PREFIX=../tmplocal/ ..
+
+# create dependency graph info
+cmake --graphviz=graphInfo.dot .
+dot -Tpng -o ../tmplocal/graphInfo.png graphInfo.dot
+
+# build system
 cmake --build . --clean-first
+
+# run one of the executables
 ./tst/uinterface  # run local build copy
 
+# install components
 cmake --build . --target install
 
+
+# cleanup
 cd ..
 rm -r ./build
 
+# display (modified) install tree
 tree tmplocal
 
