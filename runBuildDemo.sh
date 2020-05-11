63ec743787b2d534bc592e91a8cc5a0c4d84b47b
Dave Knopp <dave@stellacore.com>
2020-05-11 16:46:44 -0600

    added DEB package creation/install example

--- runBuildDemo.sh
+++ runBuildDemo.sh
@@ -3,28 +3,33 @@
 #
 
 # create and configure build environment
-mkdir ./build ./tmplocal
+mkdir ./build ./tmplocal ./tmplocal/buildInfo/
 cd build
 # would generally run ccmake here to setup configuration (use defaults here)
 cmake -DCMAKE_INSTALL_PREFIX=../tmplocal/ ..
 
 # create dependency graph info
 cmake --graphviz=graphInfo.dot .
-dot -Tpng -o ../tmplocal/graphInfo.png graphInfo.dot
+dot -Tpng -o ../tmplocal/buildInfo/graphInfo.png graphInfo.dot
 
 # build system
 cmake --build . --clean-first
 
+# generate install package
+cpack
+cp ExampleOfCMake*.deb ../tmplocal/buildInfo/
+
 # run one of the executables
 ./tst/uinterface  # run local build copy
 
-# install components
+
+# install components localy
 cmake --build . --target install
 
 
 # cleanup
 cd ..
-rm -r ./build
+# rm -r ./build
 
 # display (modified) install tree
 tree tmplocal
